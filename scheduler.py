ef589e9ab42346c3467ddb753e6eb7464abcaefc
QGJ37 <64592000+QGJ37@users.noreply.github.com>
2025-06-30 00:26:45 +0200

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -8,6 +8,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import telegram
+import os
 
 print(">>> Démarrage du bot <<<")  # Trace de démarrage
 
